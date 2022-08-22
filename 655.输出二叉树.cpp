01e529325c186b36201327a907825db2ff37e482
ranchoy <ranchoy@qq.com>
2022-08-22 22:57:20 +0800

    655.输出二叉树.cpp

---
+++ 655.输出二叉树.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.cn/problems/print-binary-tree/
+// 题解：层次遍历，注意避免数组越界情况。
+class Solution {
+public:
+    vector<vector<string>> printTree(TreeNode *root) {
+        int m = tree_depth(root), n = pow(2,m) - 1;
+        vector<vector<string>> ans(m,vector<string>(n,""));
+        build_tree_arr(root, ans, 0, (n-1)/2);
+
+        return ans;
+    }
+
+    void build_tree_arr(TreeNode *root, vector<vector<string>> &ans, int i, int j) {
+        if(root == nullptr) return;
+        int m = ans.size(), n = ans[0].size();
+        int cur_height = i + 1, k = pow(2, m-cur_height-1);
+        if(i < 0 || i >= m || j < 0 || j >= n) return;
+
+        ans[i][j] = to_string(root->val);
+        if(root->left) build_tree_arr(root->left, ans, i+1, j-k);
+        if(root->right) build_tree_arr(root->right, ans, i+1, j+k);
+    }
+
+    int tree_depth(TreeNode *root) {
+        if(root == nullptr) return 0;
+        int left_depth = tree_depth(root->left);
+        int right_depth = tree_depth(root->right);
+        return max(left_depth, right_depth) + 1;
+    }
+};
+
+/*
+[1,2]
+[1,2,3,null,4]
+
+[["","1",""],
+ ["2","",""]]
+[["","","","1","","",""],
+ ["","2","","","","3",""],
+ ["","","4","","","",""]]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
