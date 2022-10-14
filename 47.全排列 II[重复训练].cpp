b8f19c16a9affe494ba635eea063f7c8430ead18
ranchoy <ranchoy@qq.com>
2022-10-15 00:57:12 +0800

    2022/10/14

--- 47.全排列 II[重复训练].cpp
+++ 47.全排列 II[重复训练].cpp
@@ -1,6 +1,6 @@
-// https://leetcode-cn.com/problems/permutations-ii/
-// 同题：https://leetcode-cn.com/problems/permutation-ii-lcci/
-// 同题：https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof/
+// https://leetcode.cn/problems/permutations-ii/
+// 同题：https://leetcode.cn/problems/permutation-ii-lcci/
+// 同题：https://leetcode.cn/problems/zi-fu-chuan-de-pai-lie-lcof/
 // 同题：https://www.nowcoder.com/practice/fe6b651b66ae47d7acce78ffdd9a96c7?tpId=117
 // 较难想到，模拟题，每次固定一个不重复的nums[left]，再向left+1递归，注意nums数组不是引用。
 class Solution {
@@ -8,18 +8,19 @@ public:
     vector<vector<int>> res;
     vector<vector<int>> permuteUnique(vector<int> &nums) {
         sort(nums.begin(), nums.end(), less<int>());
-        perm_array(nums, 0);
+        dfs(nums, 0);
         return res;
     }
     
-    void perm_array(vector<int> nums, int left) {
-        if(left == nums.size()-1) {
+    void dfs(vector<int> nums, int left) {
+        int n = nums.size();
+        if(left == n-1) {
             res.push_back(nums);
         } else {
-            for(int i = left; i <= nums.size()-1; i++) {
+            for(int i = left; i <= n-1; i++) {
                 if(i != left && nums[left] == nums[i]) continue; // 保证nums[left]不重复, 不能用while替代
                 swap(nums[left], nums[i]);
-                perm_array(nums, left+1); // 重要
+                dfs(nums, left+1); // important
             }
         }
     }
