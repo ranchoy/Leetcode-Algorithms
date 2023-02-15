da36e9d064241af2007696808c03fa3d56bf1d41
ranchoy <ranchoy@qq.com>
2023-02-15 23:53:33 +0800

    2023/02/15

---
+++ 单调栈/42.接雨水.java
@@ -0,0 +1,37 @@
+// https://leetcode.cn/problems/trapping-rain-water/
+// 题解：单调栈，栈空间递减，遇到比栈顶大的height[i]计算雨水，注意会遇到21112平顶和1112接不住雨水的情况。
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0;
+        int n = height.length;
+        Deque<Integer> stk = new ArrayDeque<>();
+
+        for(int i = 0; i < n; i++) {
+            while(!stk.isEmpty() && height[stk.peek()] < height[i]) {
+                int k = stk.pop();
+                while(!stk.isEmpty() && height[stk.peek()] == height[k]) {
+                    stk.pop(); // 21112
+                }
+
+                if(!stk.isEmpty()) {
+                    int l = i - stk.peek() - 1; // 避免1112
+                    int h = Math.min(height[i], height[stk.peek()]) - height[k];
+                    ans += h * l;
+                }
+            }
+            stk.push(i);
+        }
+
+        return ans;
+    }
+}
+
+/*
+[2,1,1,1,2]
+[0,1,0,2,1,0,1,3,2,1,2,1]
+[4,2,0,3,2,5]
+
+3
+6
+9
+*/
